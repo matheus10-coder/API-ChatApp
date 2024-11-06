c8be5ac43e065bc16aa2deffe13a102b7d0cac4e
mribeiro10-coder <mfebribeiro@gmail.com>
2024-11-05 20:34:06 -0600

    Full package deployed

---
+++ src/test/kotlin/com/kotlin/matech/chatapp/ChatappApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.matech.chatapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
